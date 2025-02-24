06b508b6457b66a9fefa399ce5350639692ffdd3
Kennedy Wee <kennedyweesupitang@gmail.com>
2025-02-24 23:39:27 +0800

    adapter vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-vite: {
+  vite: {
     plugins: [tailwindcss()],
   },
-    // add yur domain name here
-   site: 'https://yoursite.com',
-  integrations: [sitemap()]
+  // add yur domain name here
+  site: "https://lumetrika.com",
+  integrations: [sitemap()],
+  output: "static",
+  adapter: vercel({
+    imageService: true,
+  }),
 });
